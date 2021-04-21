4336a54750825158260e395ef32197719034cbf3
Len Smith <len@barrison.com>
2021-04-20 20:43:18 -0700

    Better spacing and colors in TODO

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,7 +7,17 @@ type PropTypes = {
 
 const Todo = ({ todo }: PropTypes) => {
   console.log('🦄 - todo', todo);
-  return <div className={`todo ${todo.state}`}>{todo.description}</div>;
+  return (
+    <div className={`todo ${todo.state}`}>
+      <span className="checkbox-wrapper">
+        <input type="checkbox" checked={todo.isComplete}></input>
+      </span>
+      <span className="description">{todo.description}</span>
+      {!!todo.dueDate && (
+        <span className="due-date">{todo.dueDate.toLocaleDateString('en-US')}</span>
+      )}
+    </div>
+  );
 };
 
 export default Todo;
